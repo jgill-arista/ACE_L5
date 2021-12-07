5b2217ee69324e08e829657cc2199ef1b9c6aa9a
John Gill <jgill@arista.com>
2021-12-07 15:55:20 +0000

    Initial commit

---
+++ API/L3_config.py
@@ -0,0 +1,12 @@
+import pyeapi
+
+connect = pyeapi.connect_to("leaf1-DC1")
+
+connect.api("ipinterfaces").create('Ethernet4')
+result = connect.api("ipinterfaces").set_address('Ethernet4', '4.4.4.4/24')
+
+if result == True:
+    print("completed!")
+if result == False:
+    print("error!")
+
